5c1292f4bc894f7b334c44ff41af24dfa896a510
sodykovtagir-cpu <sodykovtagir-cpu@users.noreply.github.com>
2026-08-28 14:40:33 +0000

    Fix FileIcon to use IsDragging flag

--- Assets/Scripts/Assembly-CSharp/PC/Component/Software/FileIcon.cs
+++ Assets/Scripts/Assembly-CSharp/PC/Component/Software/FileIcon.cs
@@ -68,7 +68,7 @@ namespace PC.Component.Software
         {
             if (eventData == null) return;
             if (eventData.dragging) return;
-            if (DesktopIconDragger.WasDragged) return;
+            if (DesktopIconDragger.IsDragging) return;
             var cb = callback;
             if (cb != null) cb(File);
         }
